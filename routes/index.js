1455dcb926997e51619df33c8a33c2ad490f8693
TomasAltuzarra <e.tomasaltuzarra@gmail.com>
2023-10-29 18:18:58 -0300

    added nav components

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,19 @@
 var express = require("express");
 var router = express.Router();
+const {requiresAuth} = require('express-openid-connect')
 
 router.get("/", (req, res) => {
-    console.log(req.oidc.isAuthenticated());
+    //console.log(req.oidc.isAuthenticated());
     res.render("index", {
         title: "Express Demo", 
+        isAuthenticated: req.oidc.isAuthenticated(),
+        user: req.oidc.user,
+    });
+})
+
+router.get("/secured", requiresAuth(), (req, res) => {
+    res.render("secured", {
+        title: "Secure Page", 
         isAuthenticated: req.oidc.isAuthenticated(),
         user: req.oidc.user,
     });
